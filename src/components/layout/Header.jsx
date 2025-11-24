6de1f103d783381c2823a7bb8edc081597e26607
MariaBejenaru33 <mbejenaru30@yahoo.com>
2025-11-24 20:09:10 +0200

    Am facut modificari

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,9 +1,14 @@
 import { Link } from "react-router-dom";
 
+import sfLogo from "../../assets/images/sf1.png";
+
 export default function Header() {
   return (
     <header className="header-container">
-      <h1 className="header-logo">SalonFinder</h1>
+      <div className="header-logo-container">
+        <img src={sfLogo} alt="SalonFinder Logo" className="header-logo-img" />
+        <h1 className="header-logo">SalonFinder</h1>
+      </div>
 
       <nav className="header-nav">
         <Link to="/" className="header-link">
